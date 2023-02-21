8c1bfad02f51c03e463d3640713bf64ad63a2e22
Pierre-Loup Cabarat <pcabarat@insa-rennes.fr>
2023-02-21 11:30:32 +0100

    Create an env.mak file to setup a virtual env

--- build.sh
+++ build.sh
@@ -25,8 +25,8 @@ mkdir -p $INCDIR
 cp -r $SLHDRLIB $PREFIX
 cp -r $SLHDRINC $PREFIX
 
-export LD_LIBRARY_PATH=$LIBDIR
-export PKG_CONFIG_PATH=$LIBDIR/pkgconfig/
+export LD_LIBRARY_PATH=$LIBDIR:$LD_LIBRARY_PATH
+export PKG_CONFIG_PATH=$LIBDIR/pkgconfig:$PKG_CONFIG_PATH
 
 cd $SLDIR
 ./configure --srcdir=$SLHDRDIR --incdir=$SLHDRINC --prefix=$PREFIX
@@ -54,3 +54,57 @@ meson configure build --prefix=$PREFIX --libdir=$LIBDIR
 meson compile -C build
 meson install -C build
 
+cat > $BUILDROOT/env.mak  << EOF
+
+3ems2_exit_venv(){
+  source \$tmpfile
+  rm \$tmpfile
+  unset \$tmpfile
+  unset 3ems2_bck_create
+  unset 3ems2_exit_venv
+  unset setup_3ems2_env
+}
+
+#Create backup file to restore env when exited
+3ems2_bck_create() {
+  tmpfile=\$(mktemp -t env_XXX)
+  trap 3ems2_exit_venv INT QUIT EXIT
+
+  cat > \$tmpfile  << EOF2
+LD_LIBRARY_PATH=\$LD_LIBRARY_PATH
+PKG_CONFIG_PATH=\$PKG_CONFIG_PATH
+PATH=\$PATH
+PS1='\$PS1'
+EOF2
+}
+
+setup_3ems2_env(){
+
+  3ems2_bck_create
+
+  local BUILDROOT=$BUILDROOT
+  local PREFIX=${PREFIX}
+
+  local SLHDRDIR=${SLHDRDIR}
+
+  local SLHDRINC=${SLHDRINC}
+  local SLHDRLIB=${SLHDRLIB}
+
+  local FFDIR=$FFDIR
+  local OVDIR=$OVDIR
+  local MPDIR=$MPDIR
+  local SLDIR=$SLDIR
+
+  local LIBDIR=${LIBDIR}
+  local INCDIR=${INCDIR}
+
+  export LD_LIBRARY_PATH=$LIBDIR:\$LD_LIBRARY_PATH
+  export PKG_CONFIG_PATH=$LIBDIR/pkgconfig:\$PKG_CONFIG_PATH
+  export PATH=$PREFIX/bin:\$PATH
+
+  PS1='(3ems2) '\$PS1
+}
+
+setup_3ems2_env
+
+EOF
